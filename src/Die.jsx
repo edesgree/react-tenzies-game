7857ae2ed5c8f2db29ba32f49fa03266ec6293c1
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-15 02:11:44 +0100

    grid css to draw dice values

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,14 +3,14 @@
 export default function Die(props) {
   const dots = [];
   for (let i = 0; i < props.value; i++) {
-    dots.push(<span className="dot">.</span>);
+    dots.push(<span className="dot"></span>);
   }
   return (
     <div
+      value={props.value}
       onClick={props.holdDice}
-      className={props.isHeld ? 'isheld die' : 'die'}
+      className={`${props.isHeld ? 'isheld' : ''} die value-${props.value}`}
     >
-      {props.value}
       {dots}
     </div>
   );
