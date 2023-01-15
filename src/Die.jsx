69f1e8d107059b9bfb96753a66584fe961e877fe
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-15 01:35:56 +0100

    create span element for dots

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,11 +1,17 @@
 //const [isMatch, setIsMatch] = React.useState(false)
 
 export default function Die(props) {
+  const dots = [];
+  for (let i = 0; i < props.value; i++) {
+    dots.push(<span className="dot">.</span>);
+  }
   return (
-    <div>
-      <button onClick={props.holdDice} className={props.isHeld ? 'isheld' : ''}>
-        {props.value}
-      </button>
+    <div
+      onClick={props.holdDice}
+      className={props.isHeld ? 'isheld die' : 'die'}
+    >
+      {props.value}
+      {dots}
     </div>
   );
 }
